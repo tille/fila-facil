74861a3b4dd574fa446c5ae7821aff40bb4e7871
Jorman Bustos <tille100@gmail.com>
2013-09-09 10:28:56 -0500

    testing

--- index.php
+++ index.php
@@ -5,12 +5,33 @@
 <body>
 
   <?php
-    $con = mysqli_connect("localhost","root","root","ff_database");
-    $name = 'Don reymon';
-    $sql = "SELECT * FROM test WHERE name='$name'";
-    $result = mysqli_query($con,$sql);
-    $datos = mysqli_fetch_array($result);
-    echo $datos['name'];
+      $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+      $server = $url["host"];
+      $username = $url["user"];
+      $password = $url["pass"];
+      $db = substr($url["path"],1);
+
+      mysql_connect($server, $username, $password);
+      mysql_select_db($db);
+      
+      $name = 'Don reymon';
+      $sql = "SELECT * FROM test WHERE name='$name'";
+      $result = mysqli_query($con,$sql);
+      $datos = mysqli_fetch_array($result);
+      echo $datos['name'];
+      
+  ?>
+
+  <?php
+    // DATABASE_URL: mysql://b251ba2fc71a0a:44422efc@us-cdbr-east-04.cleardb.com/heroku_4b016d38f7d5c5b?reconnect=true
+
+    // $con = mysqli_connect("localhost","root","root","ff_database");
+    // $name = 'Don reymon';
+    // $sql = "SELECT * FROM test WHERE name='$name'";
+    // $result = mysqli_query($con,$sql);
+    // $datos = mysqli_fetch_array($result);
+    // echo $datos['name'];
   ?>
   
 </body>
