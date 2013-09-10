25e154377a457f3803ddc0eeb0696f46ed288612
Jorman Bustos <tille100@gmail.com>
2013-09-10 18:36:37 -0500

    ficing the .gitignore bug

---
+++ app/models/turn.php
@@ -0,0 +1,20 @@
+<?php
+  include 'db/DAO/DAO_turn.php';
+
+  class Turn {
+    var $module, $number = null;
+    
+    function Turn($mod){
+      $this->module = $mod;
+    }
+    
+    function read(){
+      $this->number = DAO_turn::DAO_read($this->module);
+      return $this->number;
+    }
+    
+    function update(){
+      return DAO_turn::DAO_update($this->module, $this->number);
+    }
+  }
+?>
