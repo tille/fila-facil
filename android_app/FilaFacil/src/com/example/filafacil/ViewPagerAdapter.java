0a7a627023112f179e8da5bf1ea9a7407bc5a5de
Santiago Vanegas <savanegasg@gmail.com>
2013-09-10 19:39:21 -0500

    Android view developed

---
+++ android_app/FilaFacil/src/com/example/filafacil/ViewPagerAdapter.java
@@ -0,0 +1,56 @@
+package com.example.filafacil;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.util.Log;
+ 
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+ 
+    // Declare the number of ViewPager pages
+    private AdmisionesFragment admisiones;
+    private CarteraFragment cartera;
+    private CajaFragment caja;
+    private CertificadosFragment certificados;
+    final int PAGE_COUNT = 4;
+ 
+    public ViewPagerAdapter(FragmentManager fm, ValuesManager valores) {
+        super(fm);
+        admisiones = new AdmisionesFragment(valores);
+        cartera = new CarteraFragment(valores);
+        caja = new CajaFragment(valores);
+        certificados = new CertificadosFragment(valores);
+        Log.d("CONSOLA", "ViewPagerAdapter constructor");
+    }
+ 
+    @Override
+    public Fragment getItem(int arg0) {
+        switch (arg0) {
+ 
+        // Open FragmentTab1.java
+        case 0:
+            return admisiones;
+ 
+        // Open FragmentTab2.java
+        case 1:
+            //CarteraFragment cartera = new CarteraFragment();
+            return cartera;
+ 
+        // Open FragmentTab3.java
+        case 2:
+            //CajaFragment caja = new CajaFragment();
+            return caja;
+        case 3:
+        	//CertificadosFragment certificados = new CertificadosFragment();
+        	return certificados;
+        }
+        return null;
+    }
+ 
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return PAGE_COUNT;
+    }
+ 
+}
