2ba6b61b4dcc58f7c5adb3957118603041be459d
Jorman Bustos <tille100@gmail.com>
2013-09-11 18:02:40 -0500

    deleting return values cuz we we'll work printing the results when the web service is called

--- config/routes.php
+++ config/routes.php
@@ -7,6 +7,7 @@
 
   function calling($id, $params){
     $params = explode(',',$params);
-    if($id==0) turn_controller::get_turn($params[0]);
+    if($id==0) return turn_controller::get_turn($params[0]);
+    return "";
   }
 ?>
--- services.php
+++ services.php
@@ -3,9 +3,10 @@
 
   if( isset($_GET["q"]) && isset($routes[$_GET["q"]]) ){
     $params = (isset($_GET["params"]))?$_GET["params"]:"";
-    return calling($routes[$_GET["q"]], $params);
+    $response = calling($routes[$_GET["q"]], $params);
+    if( $response == "" ) echo "Error";
+    else echo $response;
   }else{
-    echo "Please specify a valid service!";
-    return "Please specify a valid service!";
+    echo 'Error';
   }
 ?>
