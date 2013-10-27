f342c594d3a3db804661f567b0705216ba1c9e87
Esteban Foronda Sierra <seforonda@gmail.com>
2013-10-27 09:26:12 +0100

    Changing operator state dona

--- app/views/user/logout.php
+++ app/views/user/logout.php
@@ -1,5 +1,8 @@
 <?php
+  ob_start(); 
   session_start();
+  $id = $_SESSION['id'];
+  $pwd = $_SESSION['password'];
   session_destroy();
-  header('Location: '."http://localhost:8888/ff/index.php");
+  $success = header('Location: '."http://localhost:8888/ff/services.php?q=change_operator_state&params=".$id.",0,".$pwd);
 ?>
